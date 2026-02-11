1e9d96a05d5456e06234ec0f4832614a035d7fa5
aero <zh372xg@gmail.com>
2026-02-11 19:33:56 +0700

    Enhance bot with image commands and duck API
    Added new commands for sending images and fetching a duck image URL.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,7 +9,7 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'we have logged in as {bot.user}')
+    print(f'you have logged in as {bot.user}')
 
 @bot.command()
 async def hello(ctx):
@@ -19,7 +20,30 @@ async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def heh(ctx,*,message):
+async def say(ctx,*,message):
     await ctx.send(message)
 
-bot.run("TOKEN")
+@bot.command()
+async def mem1(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def animals(ctx):
+    with open('images/animals.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run('DISCORD_TOKEN')
